4759073a7f3d3e2068cb62baa352887cbed69fb8
Isaac Thiessen <isaac.thiessen31@online.camosun.ca>
2018-05-28 14:51:24 -0700

    Mostly finished login page

---
+++ public_html/login-verify.php
@@ -0,0 +1,70 @@
+<html>
+<h1>Login verification page</h1>
+<?php
+function check_login ($dbc, $email = '', $pass = '') {
+
+	//This function checks login credentials and returns an array
+	//  array ( bool, arr )
+	// If the bool is true, the login was successfull and the array is the customers first name and email
+	// if the bool is false, the array is a list of errors that occured. Unsuccessfull logon is an error.	
+		
+	
+	$errors = array();
+
+	//checking information was entered
+	if (empty($email)){
+		array_push ($errors, 'Please enter email');
+	} else {
+		//password was entered
+		if ( ! preg_match('/^(\w|\.)+@(\w|\.)+\.[a-z]+$/i', $email, $match)){
+			array_push ($errors, 'Invalid Email');
+		} else { 
+
+			$e = mysqli_real_escape_string($dbc, trim($email)); 
+		}
+
+	}
+	if (empty($pass)) {
+		array_push($errors, 'Please enter password');
+	} else {
+		$p = mysqli_real_escape_string($dbc, md5(trim($pass)));
+	}
+	
+	//checking to see if we got this far
+	if ( empty($errors)){
+
+		//no errors so far
+		//now we retrieve info from db
+		$query = "SELECT fname FROM ICS199Group07_dev.CUSTOMERS WHERE email = '$e' AND passwd = '$p'";
+		$r = @mysqli_query($dbc, $query);
+
+		//checking results
+		if (mysqli_num_rows($r) != 1){
+			
+			//error wrong number of rows returned, user doesn't exist in database
+			array_push ($errors, 'Wrong email or password');
+		} else {
+			//USER EXISTS IN DATABASE!!!
+			$row = mysqli_fetch_array ( $r, MYSQLI_ASSOC);
+			return array(true, $row);
+		}
+	}
+	return array( false, $errors);
+}
+$username = 'isaac.thiessen31@online.camosun.ca';
+$password = '1234asdf';
+$connection = new mysqli("localhost", "cst170","381953","ICS199Group07_dev");
+$results = check_login($connection, $email = $username, $pass = $password);
+
+
+if ( $results[0] ){
+	foreach ( $results[1] as &$i) {
+		echo "Log in successful! Welcome " . $i . "!";
+	}
+} else {
+	echo "Log in failed: ";
+	foreach ( $results[1] as &$i) {
+		echo $i . ".";
+	}
+}
+?>
---
+++ public_html/login.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+<h1>LOGIN PAGE!!</h1>
+
+<form action="login-verify.php" method="POST" enctype="multipart/form-data">
+<p> Username: <input type="text" name="username" id='usrname'></p>
+<p> Password: <input type="text" name="password" id='passwd'></p>
+</select></p>
+<input type="submit" value="submit" id="submit1" />
+</form>
