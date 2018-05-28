338c7d61a728b981f75d9a0430af81f4b8b73580
Brendon Waters <brendon.waters117@gmail.com>
2018-05-28 15:06:44 -0700

    new product page for each product

--- public_html/product.php
+++ public_html/product.php
@@ -4,15 +4,7 @@
 		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="./css.css">
 		<title>Name of Product</title>
-			<header>
-				<h1>The mlik header image goes here!</h1>
-				<ul id = "navbar">
-					<li><a href="index.php">Home</a></li>
-					<li><a href="products.php">Products</a></li>
-					<li><a href="cart.php">Cart</a></li>
-					<li><a href="about.php">About</a></li>
-				</ul>
-			</header>
+		<?php include 'navbar.php'; ?>
 				<?php
 	//session_start();	//start session
 	//echo session_id();	//debug session
@@ -25,21 +17,23 @@
 	}
 	?>
 	</head>
-
+	
 	<body>
+	<p> Product: </p>
 		<div class='product'>
 		
 		<!-- subtle but important break -->
-		<br>
-		
+	<?php
+		$queryStr = "SELECT * FROM PRODUCTS WHERE prod_id = 1;";
+		$query = $connection->query($queryStr);
+	?>	
 		<!-- Here we retrieve the image based on the product id. The product with a product id of 1 will retrieve 1.jpg from the product_pics directory -->
-		<img class='prod_img' src='product_pics/<?php echo $dataCat['prod_id'];?>.jpg' alt=<?php echo $dataCat['Name'];?> >	
+		<img class='prod_img' src='product_pics/<?php echo $product_id?>.jpg'>
 
-		<p class='prod_txt'><b><?php print $dataCat['Name'];?></b></p> 
-		<p class='prod_txt'>$ <?php print $dataCat['Price'];?></p>
-		</div><img class='prod_img'
+		<p class='prod_txt'><b><?php print $query['Name'];?></b></p> 
+		<p class='prod_txt'><?php print $query['Price'];?></p>
+		</div>
 	</body>
-
 	<footer>
 	  <hr>
 	  We sell quality goods! Check us out on Twitter, Social Media Site 1, Social Media Site 2
