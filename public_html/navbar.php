568984ff7999574943ac97d15e5984183de2366c
Brendon Waters <brendon.waters117@gmail.com>
2018-05-29 11:38:12 -0700

    Added login and add product to header

--- public_html/navbar.php
+++ public_html/navbar.php
@@ -5,7 +5,9 @@
 					<li><a href=\"index.php\">Home</a></li>
 					<li><a href=\"products.php\">Products</a></li>
 					<li><a href=\"cart.php\">Cart</a></li>
-					<li><a href=\"about.php\" id=\"active\">About</a></li>
+					<li><a href=\"about.php\">About</a></li>
+                                        <li><a href=\"login.php\">Login</a></li>
+                                        <li><a href=\"productForm.php\">Add Product</a></li>
 				</ul>
 			</header>";
 			
