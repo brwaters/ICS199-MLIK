fa780cb49244a17b38af86f5dce03a378fcf3f64
jwarnes <jacob-warnes@hotmail.com>
2018-05-29 11:58:34 -0700

    navbar

--- public_html/navbar.php
+++ public_html/navbar.php
@@ -5,11 +5,20 @@
 					<li><a href=\"index.php\">Home</a></li>
 					<li><a href=\"products.php\">Products</a></li>
 					<li><a href=\"cart.php\">Cart</a></li>
-					<li><a href=\"about.php\" id=\"active\">About</a></li>
+					<li><a href=\"about.php\">About</a></li>
 				</ul>
 			</header>";
-			
+	$reg = "index\.php";
+	$swap = "index\.php\" > here it is";
+	
+	$string = 'April 15, 2003';
+	$pattern = '/(\w+) (\d+), (\d+)/i';
+	$replacement = "$page";
+	
+	echo preg_replace($pattern, $replacement, $string);
+	//$nav1 = preg_replace($reg, $swap, $nav);
 	echo $nav;
+	echo $nav1;
 
 
 ?>
