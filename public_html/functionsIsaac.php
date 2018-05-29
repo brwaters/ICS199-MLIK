6788b71d8851898260f637d2007d7cbd76b987cc
Isaac Thiessen <isaac.thiessen31@online.camosun.ca>
2018-05-29 15:48:03 -0700

    USERS CAN NOW LOGIN!!!!! SESSION VARIABLES WORK!!!!

--- public_html/functionsIsaac.php
+++ public_html/functionsIsaac.php
@@ -46,4 +46,62 @@ function checkImage( $image ){
 	} 
 }
 
+function getConnection () {
+$connection =  new mysqli("localhost", "cst170","381953","ICS199Group07_dev");	
+return $connection;
+}
+
+function check_login ($dbc, $email = '', $pass = '') {
+
+	//This function checks login credentials and returns an array
+	//  array ( bool, arr )
+	// If the bool is true, the login was successfull and the array is the customers first name and email
+	// if the bool is false, the array is a list of errors that occured. Unsuccessfull logon is an error.	
+		
+	
+	$errors = array();
+
+	//checking information was entered
+	if (empty($email)){
+		array_push ($errors, 'Please enter email');
+	} else {
+		//password was entered
+		if ( ! preg_match('/^(\w|\.)+@(\w|\.)+\.[a-z]+$/i', $email, $match)){
+			array_push ($errors, 'Invalid Email');
+		} else { 
+
+			$e = mysqli_real_escape_string($dbc, trim($email)); 
+		}
+
+	}
+	if (empty($pass)) {
+		array_push($errors, 'Please enter password');
+	} else {
+		$p = mysqli_real_escape_string($dbc, md5(trim($pass)));
+	}
+	
+	//checking to see if we got this far
+	if ( empty($errors)){
+
+		//no errors so far
+		//now we retrieve info from db
+		$query = "SELECT fname, cust_id, account_type FROM ICS199Group07_dev.CUSTOMERS WHERE email = '$e' AND passwd = '$p'";
+		$r = @mysqli_query($dbc, $query);
+
+		//checking results
+		if (mysqli_num_rows($r) != 1){
+			
+			//error wrong number of rows returned, user doesn't exist in database
+			array_push ($errors, 'Wrong email or password');
+		} else {
+			//USER EXISTS IN DATABASE!!!
+			$row = mysqli_fetch_array ( $r, MYSQLI_ASSOC);
+			return array(true, $row);
+		}
+	}
+	return array( false, $errors);
+}
+function typethis(){
+	echo 'FUDSLKFJIDSHFPIDSHKJFHDSFFIUGDSFIDSHF';
+}
 ?>
--- public_html/login.php
+++ public_html/login.php
@@ -3,14 +3,49 @@
 <meta charset="utf-8">
 <link rel="stylesheet" href="./css.css">
 <title>home</title>
-			<?php include 'navbar.php'; ?>
+			<?php include 'functionsIsaac.php'; include 'navbar.php'; ?>
 </head>
 <body>
 <h1>LOGIN PAGE!!</h1>
 
-<form action="login-verify.php" method="POST" enctype="multipart/form-data">
+<form action="login.php" method="POST" enctype="multipart/form-data">
 <p> Username: <input type="text" name="username" id='usrname'></p>
-<p> Password: <input type="text" name="password" id='passwd'></p>
+<p> Password: <input type="password" name="password" id='passwd'></p>
 </select></p>
 <input type="submit" value="submit" id="submit1" />
 </form>
+
+<?php
+
+//variables
+$conn = getConnection();
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
+
+//only runs if there has been input
+if (sizeOf($_POST) > 0 ){
+
+	$login = check_login($conn, $email = $user, $pass = $pass);
+	
+
+	if ($login[0]){
+		//login successful [1] is the name
+
+		echo 'Welcome ' . $login[1]['fname'] . '!';
+		
+		//setting session variables
+		session_start();
+		$_SESSION['loggedIn'] = true;
+		$_SESSION['cust_id'] = $login[1]['cust_id'];
+		$_SESSION['fname'] = $login[1]['fname'];
+		$_SESSION['account_type'] = $login[1]['account_type'];
+			
+
+	} else {
+		//login failed [1] is an array of errors
+
+		echo errorHandler($login[1]);
+	}
+}
+?>
