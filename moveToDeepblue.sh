146cf8a79e2daee7db654745896966d725709c62
Isaac Thiessen <isaac.thiessen31@online.camosun.ca>
2018-06-05 10:36:55 -0700

    added backdrop for header

--- moveToDeepblue.sh
+++ moveToDeepblue.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 git pull
 tar cvf public_html.tar.gz public_html/
-scp public_html.tar.gz cst166@deepblue.cs.camosun.bc.ca:public_html.tar.gz
+
+
+ping -c 1 mlik.ra
+if [ 1 -eq 0 ] 
+then
+	scp public_html.tar.gz root@mlik.ra:public_html.tar.gz
+	ssh root@mlik.ra '~/unpack.sh'
+else
+	echo 'local mlik not pingable'
+fi
+
+
+ping -c 1 deepblue.cs.camosun.bc.ca
+if [ $? -eq 0 ] 
+then
+	scp public_html.tar.gz cst166@deepblue.cs.camosun.bc.ca:public_html.tar.gz
+	ssh cst166@deepblue.cs.camosun.bc.ca '~/unpack.sh'
+else
+	echo 'Deepblue not pingable'
+fi
