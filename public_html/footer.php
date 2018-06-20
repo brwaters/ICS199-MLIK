dac2b9359e3076765d20981d4b1aea1fe433301b
jwarnes <jacob-warnes@hotmail.com>
2018-06-20 14:27:17 -0700

    footer fixes'

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,10 +1,11 @@
 <?php
-$nav = '<div id=\"footer\">
+$nav = '
     <footer>
-	  <hr>
+	  <div id="foot">
 	  <p>We sell quality goods! Check us out on Twitter, Social Media Site 1, Social Media Site 2</p>
 	  <p><a href="privacyPolicy.php">Privacy Policy</a></p>
+	  </div>
 	</footer>
-        </div>';
+        ';
 echo $nav;
 ?>
