eb769a9e6c6b305a176d831c2e6a810652d126e1
aal704 <32022418+aal704@users.noreply.github.com>
2018-06-25 09:49:58 -0700

    changed names on footer

--- public_html/footer.php
+++ public_html/footer.php
@@ -2,7 +2,7 @@
 $nav = '
     <footer>
 	  <div id="foot">
-	  <p>We sell quality goods! Check us out on Twitter, Social Media Site 1, Social Media Site 2</p>
+	  <p>We sell quality goods! Check us out on Twitter, Facebook, and Instagram!</p>
 	  <p><a href="privacyPolicy.php">Privacy Policy</a></p>
 	  </div>
 	</footer>
